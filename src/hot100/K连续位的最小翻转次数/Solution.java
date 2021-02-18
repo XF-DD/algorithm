f5e65426ab1ae22233c20432f5f0e5fdbcf8a4ac
Bnmzy <992E8FE835900BD6953773D328091078@i-search.com.cn>
2021-02-18 16:19:45 +0800

    010218

---
+++ src/hot100/K连续位的最小翻转次数/Solution.java
@@ -0,0 +1,8 @@
+package hot100.K连续位的最小翻转次数;
+
+/**
+ * @Author: XF-DD
+ * @Date: 21/02/18 16:18
+ */
+public class Solution {
+}
--- src/hot100/下一个排列/Solution.java
+++ src/hot100/下一个排列/Solution.java
@@ -1,11 +1,18 @@
 package hot100.下一个排列;
 
+import java.util.Iterator;
+
 /**
  * @Author: XF-DD
  * @Date: 20/08/22 12:33
  */
-public class Solution {
+public class Solution  {
     public void nextPermutation(int[] nums) {
+
+    }
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
 
     }
 }
